68bcfb925190bcf6534d2a4ab8e85b0a054aedb9
Thanh Van <tranthithanhvanbmt@gmail.com>
2026-04-10 22:35:56 +0700

    Avoid Render 500 by using non-manifest static storage

--- shareit/settings.py
+++ shareit/settings.py
@@ -103,7 +103,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
